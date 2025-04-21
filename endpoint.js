b9634254e6c7f8f4871958761aa544798bb96769
Arianna <ariigrangetto5@gmail.com>
2025-04-21 12:51:57 -0300

    changin name of key

---
+++ endpoint.js
@@ -0,0 +1,2 @@
+const KEY = "e190dc94e6d3ca2e93566413c267ec4e";
+export default KEY;
--- src/pages/hooks/useClima.js
+++ src/pages/hooks/useClima.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import KEY from "../../../key";
+import KEY from "../../../endpoint";
 
 const API_KEY = KEY;
 
